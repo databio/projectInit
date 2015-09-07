d4b24cd59663e96c06b1799541726d2624b4cecc
sheffien <sheffien@users.noreply.github.com>
2015-09-07 15:04:31 +0200

    update variable structure

--- R/project.init.R
+++ R/project.init.R
@@ -2,50 +2,50 @@
 #' All this does is source the 00-init.R script for the project,
 #' you pass a complete folder or a relative path.
 #' @export
-project.init = function(codeDir=NULL, dataDir=NULL, SHARE.DIR=getOption("SHARE.DIR")) {
-	if (is.null(getOption("SHARE.DIR"))) {
-		stop ("you must set global option(\"SHARE.DIR\") before calling project.init().");
+project.init = function(codeDir=NULL, dataDir=NULL, RESOURCES=Sys.getenv("RESOURCES")) {
+	if (is.null(Sys.getenv("RESOURCES"))) {
+		stop ("you must set global environmental variable $RESOURCES before calling project.init().");
 	}
 
-	# Set WORKING.DIR
+	# Set PROJECT.DIR
 	if (is.null(codeDir)) { # null working dir.
-		warning ("WORKING.DIR set to current dir: ", getwd());
-		WORKING.DIR=paste0(getwd(), "/");
-	} else if (substr(codeDir,1,1) != "/") {
+		warning ("PROJECT.DIR set to current dir: ", getwd());
+		PROJECT.DIR=paste0(getwd(), "/");
+	} else if (substr(codeDir,1,1) != "/" & substr(codeDir,1,1) != "~") {
 		# It is a relative path
-		if (is.null(getOption("PROJECT.CODE.BASE"))) {
+		if (is.null(Sys.getenv("CODEBASE"))) {
 			# relative path requires global base directory
-			stop("You should set a global variable PROJECT.CODE.BASE in your .Rprofile to use Nathan's shared R utils most effectively. Then you can refer to R projects with relative paths, making the code portable and sharable.");
+			stop("You should set an environment variable CODEBASE in your .bashrc to use the shared R utils most effectively. Then you can refer to R projects with relative paths, making the code portable and sharable.");
 		}
-		WORKING.DIR = paste0(getOption("PROJECT.CODE.BASE"), codeDir, "/")
+		PROJECT.DIR = paste0(Sys.getenv("CODEBASE"), codeDir, "/")
 	} else {
 		#It's a global path
-		WORKING.DIR = codeDir
+		PROJECT.DIR = codeDir
 	}
 
-	# Set PROJECT.DATA.DIR
+	# Set PROCESSED.PROJECT
 	if (is.null(dataDir)) { 
-		warning ("PROJECT.DATA.DIR set to current WORKING.DIR: ", WORKING.DIR);
-		PROJECT.DATA.DIR=WORKING.DIR
-	} else if (substr(dataDir,1,1) != "/") {
+		warning ("PROCESSED.PROJECT set to current PROJECT.DIR: ", PROJECT.DIR);
+		PROCESSED.PROJECT=PROJECT.DIR
+	} else if (substr(dataDir,1,1) != "/" & substr(dataDir,1,1) != "~") {
 		# It is a relative path
-		if (is.null(getOption("PROJECT.DATA.BASE"))) {
+		if (is.null(Sys.getenv("PROCESSED"))) {
 			# relative path requires global base directory
-			stop("You should set a global variable PROJECT.DATA.BASE in your .Rprofile to use Nathan's shared R utils most effectively. Then you can refer to R projects with relative paths, making the code portable and sharable.");
+			stop("You should set an environment variable PROCESSED in your .bashrc to use the shared R utils most effectively. Then you can refer to R projects with relative paths, making the code portable and sharable.");
 		}
-		PROJECT.DATA.DIR = paste0(getOption("PROJECT.DATA.BASE"), dataDir, "/")
+		PROCESSED.PROJECT = paste0(Sys.getenv("PROCESSED"), dataDir, "/")
 	} else {
 		#It's a global path
-		PROJECT.DATA.DIR = dataDir
+		PROCESSED.PROJECT = dataDir
 	}
 
 	# Finalize the options.
-	options(WORKING.DIR=WORKING.DIR)
-	options(PROJECT.DATA.DIR=PROJECT.DATA.DIR)
-	setwd(getOption("WORKING.DIR"));
-	message("WORKING.DIR:", getOption("WORKING.DIR"));
-	message("PROJECT.DATA.DIR:", getOption("PROJECT.DATA.DIR"));
-	# Set PROJECT.DATA.DIR
+	options(PROJECT.DIR=PROJECT.DIR)
+	options(PROCESSED.PROJECT=PROCESSED.PROJECT)
+	setwd(getOption("PROJECT.DIR"));
+	message("PROJECT.DIR:", getOption("PROJECT.DIR"));
+	message("PROCESSED.PROJECT:", getOption("PROCESSED.PROJECT"));
+	# Set PROCESSED.PROJECT
 
 	init.dirs();
 	init.options();
@@ -54,7 +54,7 @@ project.init = function(codeDir=NULL, dataDir=NULL, SHARE.DIR=getOption("SHARE.D
 
 	# Finalize the initialization by sourcing the project-specific
 	# initialization script
-	project.scripts = c(paste0(getOption("WORKING.DIR"), "src/00-init.R"), paste0(getOption("WORKING.DIR"), "projectInit.R"))
+	project.scripts = c(paste0(getOption("PROJECT.DIR"), "src/00-init.R"), paste0(getOption("PROJECT.DIR"), "projectInit.R"))
 	initialized=FALSE;
 	for (project.script in project.scripts) {
 		if (file.exists(project.script)) {
@@ -76,17 +76,42 @@ go = project.init
 
 #' Helper alias to re-run init script, using your current dir settings.
 project.refresh = function() { 
-	project.init2(codeDir=getOption("WORKING.DIR"), dataDir=getOption("PROJECT.DATA.DIR"), SHARE.DIR=getOption("SHARE.DIR"))
+	project.init2(codeDir=getOption("PROJECT.DIR"), dataDir=getOption("PROCESSED.PROJECT"), RESOURCES=Sys.getenv("RESOURCES"))
 }
 
 #' Helper alias for the common case where the data and code dirs share
 #' a name.
 #' @export
 project.init2 = function(codeDir) {
-	project.init(codeDir=codeDir, dataDir=codeDir, SHARE.DIR=getOption("SHARE.DIR"))
+	project.init(codeDir=codeDir, dataDir=codeDir, RESOURCES=Sys.getenv("RESOURCES"))
 }
 
 
+#If you make changes to a utility script and want to reload it, this will reset all the utilities, and reset you to the CWD.
+renewProject = function() {
+	clearLoadFunctions("PROJECT.VARS");
+	clearLoadFunctions("SHARE.VARS");
+	loadedUtilities = unique(getOption("LOADED.UTILITIES"))
+	options(LOADED.UTILITIES=NULL)
+	saveWorkingDir = getwd();
+	tryCatch( { source(getOption("PROJECT.INIT")); }, 
+		error = function (e) { 
+			message("Sourcing project.init failed:", e) 
+		} )
+	utToLoad = setdiff(loadedUtilities, unique(getOption("LOADED.UTILITIES")))
+	lapply(utToLoad, utility); #reload previously loaded utilities
+	setwd(saveWorkingDir);
+	message(saveWorkingDir);
+}
+#rp = renewProject;
+
+#@export
+rp= function() {
+	if (is.null(getOption("PROJECT.DIR"))) {
+		stop("No loaded project.")
+	}
+	project.init(getOption("PROJECT.DIR"), getOption("PROCESSED.PROJECT"))
+}
 
 #' This is Nathan's custom utility loading function.
 #' It goes with my R utility package, and is used to load up utilities from
@@ -95,7 +120,7 @@ project.init2 = function(codeDir) {
 #' so it's more natural if this loading function is here.
 #'
 #' It will search first any passed dir, then the working dir, and finally
-#' the global SHARE.RUTIL.DIR for the script to load.
+#' the global RGENOMEUTILS for the script to load.
 #'
 #' Should probably be renamed in the future.
 #'
@@ -104,7 +129,7 @@ project.init2 = function(codeDir) {
 #' @export
 utility = function(utilities, utilityDir="") {
 	#build a list of ordered directories to search for the utility.
-	utilityDirs = c(utilityDir, getOption("WORKING.DIR"), getOption("SHARE.RUTIL.DIR"));
+	utilityDirs = c(utilityDir, getOption("PROJECT.DIR"), paste0(getOption("RGENOMEUTILS"), "R/"));
 	for (u in utilities) {
 		foundUtility = FALSE;
 		#looks for a directory with the utilities; loads it in priority order.
@@ -125,6 +150,20 @@ utility = function(utilities, utilityDir="") {
 	}
 }
 
+loadAllUtilities = function(utilityDir=getOption("RGENOMEUTILS")) {
+	utilities = list.files(utilityDir, pattern=".R$")
+	utility(utilities, utilityDir);
+}
+
+
+#just a quick helper function to run a grep on the shared utility directory to locate a function if you don't know where the utility is.
+#should also test getOption("WORKING.DIR") ??
+findUtility = function(string) {
+	cmd = paste0('grep ', string, ' ', getOption("RGENOMEUTILS"), "R/*.R")
+	message(cmd);
+	res = system(cmd, intern=TRUE);
+	res;
+}
 
 
 #######################################################################
@@ -135,20 +174,35 @@ utility = function(utilities, utilityDir="") {
 
 #'@export
 nenv = function() {
-nShareOptionsList = c("SHARE.DIR", "SHARE.RUTIL.DIR", "SHARE.RCACHE.DIR", "SHARE.DATA.DIR","RCACHE.DIR", "RBUILD.DIR", "ROUT.DIR", "PROJECT.INIT", "PROJECT.DATA.DIR", "PROJECT.CODE.BASE", "PROJECT.DATA.BASE")
+	# env variables
+	envVars = c("RAWDATA", "PROCESSED", "RESOURCES", "WEB", "CODEBASE")
+	envVarsValues = sapply(envVars, Sys.getenv)
+	
+	nShareOptionsList = c("PROJECT.DIR", "PROJECT.INIT", 
+		"PROCESSED.PROJECT",
+		"RESOURCES.RCACHE",
+		"RCACHE.DIR",
+		"RBUILD.DIR",
+		"ROUT.DIR",
+		"RGENOMEUTILS")
 	value = sapply(nShareOptionsList, getOption)
-	cbind(value)
+	rbind(cbind(envVarsValues), cbind(value))
 }
+
+#Sys.getenv("PROCESSED")
+#Sys.getenv("CODEBASE")
+#Sys.getenv("RESOURCES")
+
 
 init.dirs = function() {
 # Set defaults:
-setOption("ROUT.DIR", paste0(getOption("PROJECT.DATA.DIR"), "results_analysis/"));
-setOption("SHARE.RCACHE.DIR", paste0(getOption("SHARE.DIR"), "cache/RCache/"));		#Global RData cache
-options(RCACHE.DIR=paste0(getOption("WORKING.DIR"), "RCache/")); 		#Project RData cache
+setOption("ROUT.DIR", paste0(getOption("PROCESSED.PROJECT"), "results_analysis/"));
+setOption("RESOURCES.RCACHE", paste0(Sys.getenv("RESOURCES"), "cache/RCache/"));		#Global RData cache
+setOption("RCACHE.DIR", paste0(getOption("PROJECT.DIR"), "RCache/")); 		#Project RData cache
 
 # Should deprecate these ones:
-setOption("SHARE.DATA.DIR", paste0(getOption("SHARE.DIR"), "data/"));			
-setOption("RBUILD.DIR", paste0(getOption("WORKING.DIR"), "RBuild/"));
+
+setOption("RBUILD.DIR", paste0(getOption("PROJECT.DIR"), "RBuild/"));
 }
 
 
@@ -181,14 +235,14 @@ rout = function(...) {
 #' Helper wrapper to get data for this project.
 #' @export
 datdir = function(...) {
-	paste0(getOption("PROJECT.DATA.DIR"), ...);
+	paste0(getOption("PROCESSED.PROJECT"), ...);
 }
 
 #' Data Dir
 #' Helper wrapper to get data for this project.
 #' @export
 resourcedir = function(...) {
-	paste0(getOption("SHARE.DIR"), ...);
+	paste0(Sys.getenv("RESOURCES"), ...);
 }
 
 
@@ -211,6 +265,7 @@ init.packages = function() {
 # Load common packages
 #######################################################################
 #Used commonly and loading quickly enough to warrant loading every time
+message("init.packages() ...");
 library(devtools)
 library(data.table, quietly=TRUE);
 library(extrafont);
@@ -221,8 +276,11 @@ library(ggplot2);
 
 init.utilities = function() {
 #######################################################################
-utility("funcCommon.R")
-utility("funcLoadSharedData.R")
+	if (! is.null(getOption("RGENOMEUTILS")) ) {
+		devtools::load_all(getOption("RGENOMEUTILS"))
+	}
+	#utility("funcCommon.R")
+	#utility("funcLoadSharedData.R")
 }
 
 
