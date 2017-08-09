4e1b0090706e81ea4c935efaa13b8fd1b9f07e2e
Nathan Sheffield <nsheff@users.noreply.github.com>
2017-08-09 17:39:48 -0400

    Delete zzz.R

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad = function(libname, pkgname) {
-  # Do some nasty side-effecting upon package startup.
-	library("devtools")
-	devtools::install_github("databio/simpleCache")
-	invisible()
-}
